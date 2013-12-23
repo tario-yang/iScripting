ce3736b906a6bc07a11e976dee1bbb297244fb4b
Yang Jun @ Windows.MacBookPro_2012_Middle <tario.yang@gmail.com>
2013-12-23 20:10:26 +0800

    Format code and refine the directory structure

--- Python/SDK_API/Simple_Test/AutoIT-SDK-Test-1.py
+++ Python/SDK_API/Simple_Test/AutoIT-SDK-Test-1.py
@@ -2,6 +2,7 @@ import Tkinter
 import win32com.client
 import win32gui
 
+
 def ACQSDK_Init():
 	hWnd = win32gui.FindWindow("TkTopLevel","SDK Testing: Live Video")
 	ret = objCSDevice.ACQSDK_Init(hWnd)
@@ -42,15 +43,17 @@ def ACQSDK_CaptureEx():
 	ret = objCSDevice.ACQSDK_CaptureEx(num,position)
 	print ret
 
+
 window_control_panel = Tkinter.Tk()
 window_live_video    = Tkinter.Tk()
 
-button_init       = Tkinter.Button(window_control_panel, text ="ACQSDK_Init", command = ACQSDK_Init).grid(row=1,column=1)
-button_uninit     = Tkinter.Button(window_control_panel, text ="ACQSDK_Uninit", command = ACQSDK_Uninit).grid(row=1,column=2)
+
+button_init       = Tkinter.Button(window_control_panel, text ="ACQSDK_Init",      command = ACQSDK_Init).grid(row=1,column=1)
+button_uninit     = Tkinter.Button(window_control_panel, text ="ACQSDK_Uninit",    command = ACQSDK_Uninit).grid(row=1,column=2)
 button_startplay  = Tkinter.Button(window_control_panel, text ="ACQSDK_StartPlay", command = ACQSDK_StartPlay).grid(row=1,column=3)
 button_pauseplay  = Tkinter.Button(window_control_panel, text ="ACQSDK_PausePlay", command = ACQSDK_PausePlay).grid(row=1,column=4)
-button_stopplay   = Tkinter.Button(window_control_panel, text ="ACQSDK_StopPlay", command = ACQSDK_StopPlay).grid(row=1,column=5)
-button_capture    = Tkinter.Button(window_control_panel, text ="ACQSDK_Capture", command = ACQSDK_Capture).grid(row=1,column=6)
+button_stopplay   = Tkinter.Button(window_control_panel, text ="ACQSDK_StopPlay",  command = ACQSDK_StopPlay).grid(row=1,column=5)
+button_capture    = Tkinter.Button(window_control_panel, text ="ACQSDK_Capture",   command = ACQSDK_Capture).grid(row=1,column=6)
 button_captureex  = Tkinter.Button(window_control_panel, text ="ACQSDK_CaptureEx", command = ACQSDK_CaptureEx).grid(row=1,column=7)
 
 
@@ -61,5 +64,6 @@ window_live_video.title("SDK Testing: Live Video")
 
 
 objCSDevice = win32com.client.Dispatch("ACQSDK.CSDevice.1")
+
 
 window_live_video.mainloop()
--- Python/SDK_API/Simple_Test/AutoIT-SDK-Test-2.py
+++ Python/SDK_API/Simple_Test/AutoIT-SDK-Test-2.py
@@ -5,6 +5,7 @@ import ctypes
 import win32api
 import os
 
+
 def WndProc(hwnd, msg, wParam, lParam):   
     if msg == WM_PAINT:   
         hdc,ps = win32gui.BeginPaint(hwnd)   
@@ -14,6 +15,7 @@ def WndProc(hwnd, msg, wParam, lParam):
         win32gui.PostQuitMessage(0)   
     return win32gui.DefWindowProc(hwnd, msg, wParam, lParam)   
 
+
 wc = win32gui.WNDCLASS()
 wc.hbrBackground = COLOR_BTNFACE
 wc.hCursor = win32gui.LoadCursor(0, IDC_ARROW)   
@@ -21,29 +23,35 @@ wc.hIcon = win32gui.LoadIcon(0, IDI_APPLICATION)
 wc.lpszClassName = "UVCSDKWINDOW"   
 wc.lpfnWndProc = WndProc
 
+
 reg = win32gui.RegisterClass(wc)   
 hwnd = win32gui.CreateWindow(
             reg,
-			'UVC ACQ - SDK Testing',
-			WS_OVERLAPPEDWINDOW,
+            'UVC ACQ - SDK Testing',
+            WS_OVERLAPPEDWINDOW,
             CW_USEDEFAULT,
-			CW_USEDEFAULT,   
+            CW_USEDEFAULT,   
             CW_USEDEFAULT,
-			CW_USEDEFAULT,   
+            CW_USEDEFAULT,   
             0,
-			0,
-			0,
-			None)
+            0,
+            0,
+            None)
+
 
 win32gui.ShowWindow(hwnd, SW_SHOWNORMAL)
 
+
 objCSDevice = win32com.client.Dispatch("ACQSDK.CSDevice.1")
+
 
 a = objCSDevice.ACQSDK_Init(hwnd)
 print a
 
+
 b = objCSDevice.ACQSDK_StartPlay()
 print b
+
 
 win32gui.UpdateWindow(hwnd)   
 win32gui.PumpMessages()
