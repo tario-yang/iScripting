23a355121e921b44edafc8f974bdc7f47ae31650
19011956 <jun.yang@carestream.com>
2015-02-05 17:16:39 +0800

    update

--- Python/SelfCollection/file_sync.py
+++ Python/SelfCollection/file_sync.py
@@ -1,5 +1,9 @@
 # coding: utf-8
 
+'''
+Implement a simple rsync
+'''
+
 import os
 import hashlib
 import shutil
@@ -7,21 +11,25 @@ import shutil
 
 class FileSync:
 
-	'''
-	Target : use this class as a simple "rsync"
-	'''
-
 	def __init__(self, source, destination):
-		self.source = source
-		self.destination = destination
 
-		retSource = self.WhatIsTarget(self.source)
-		if retSource in [1]:
-			self.ToSyncFile()
-		elif retSource in [2]:
-			self.ToSyncDirectory()
-		else:
-			print '\t*Fail to use source inputted*'
+		'''
+		Rule:
+			File -> File
+			Directory -> Directory
+		'''
+
+		self.Source = source
+		self.Destination = destination
+
+		retSource = self.WhatIsTarget(self.Source)
+		if (retSource in [1] and
+			self.ToSyncFile(self.Source, self.Destination) is None
+			) or (
+			retSource in [2] and
+			self.ToSyncDirectory(self.Source, self.Destination) is None):
+			print '\t*Incorrect parameter*'
+			print '\t\tJust allowed File to File or Directory to Directory.'
 			return None
 
 	def WhatIsTarget(self, input):
@@ -45,55 +53,67 @@ class FileSync:
 		else:
 			return 8
 
-	def ToSyncDirectory(self):
-		pass
+	def ToSyncDirectory(self, src, dest):
+		ret = self.WhatIsTarget(dest)
+		if ret in [2, 4]:
+			if ret == 2:
+				pass
+			elif ret == 4:
+		else: # illegal
+			return None
+		return False
 
-	def ToSyncFile(self):
-		ret = self.WhatIsTarget(self.destination)
-		if ret == 1: # is a file
-			destFile = self.destination
-			if FileCopy(self.source, destFile):
-				print 'Updated, "{}"'.format(destFile)
-				return True
-		elif ret == 2: # is a directlry
-			destFile = r'{0}\{1}'.format(self.destination, os.path.basename(self.source))
-			if FileCopy(self.source, destFile):
-				print 'Updated, "{}"'.format(destFile)
-				return True
-		elif ret == 4: # others, recognized as a non-existed directory
-			try:
-				os.makedirs(self.destination)
-			except Exception as e:
-				print '\t*Failed to create directory, "{0}"*\n\t\t{1}\n\t*Skipped, "{0}"*'.format(self.destination, str(e))
-				return False
-			destFile = r'{0}\{1}'.format(self.destination, os.path.basename(self.source))
-			if FileCopy(self.source, destFile):
-				print 'Copied, "{}"'.format(destFile)
-				return True
-		else: # ET!~
+	def ListDirectory(self, src):
+
+
+	def ToSyncFile(self, src, dest):
+		ret = self.WhatIsTarget(dest)
+		if ret in [1,2,4]:
+			if ret == 1: # is a file
+				if self.FileCopy(src, dest) == 1:
+					print 'Updated, "{}"'.format(dest)
+					return True
+			elif ret == 2: # is a directlry
+				destFile = r'{0}\{1}'.format(dest, os.path.basename(src))
+				if self.FileCopy(src, destFile) == 1:
+					print 'Updated, "{}"'.format(destFile)
+					return True
+			elif ret == 4: # others, recognized as a non-existed directory
+				try:
+					os.makedirs(dest)
+				except Exception as e:
+					print '\t*Failed to create directory, "{0}"*\n\t\t{1}\n\t*Skipped, "{0}"*'.format(dest, str(e))
+					return False
+				else:
+					destFile = r'{0}\{1}'.format(dest, os.path.basename(src))
+					if self.FileCopy(src, destFile) == 1:
+						print 'Copied, "{}"'.format(destFile)
+						return True
+		else: # illegal
 			return None
 		return False # All failure of FileCopy
 
 	def FileCopy(self, srcFile, destFile):
 		'''
-		True: operated
-		False: failed to operate
+		0: Skipped
+		1: Copied
+		2: Failed
 		'''
-		if IsFileSame(srcF, destFile):
+		if self.IsFileSame(srcFile, destFile):
 			print 'Skipped, "{}"'.format(srcFile)
-			return True
+			return 0
 		else:
 			try:
 				shutil.copy(srcFile, destFile)
 			except Exception as e:
 				print '\n\t*Error occurs while copying file, "{0}" to "{1}"*\n\t\t"{2}"'.format(srcFile, destFile, str(e))
-				return False
+				return 2
 			else:
-				return True
+				return 1
 
 	def IsFileSame(self, srcFile, destFile):
 		if self.WhatIsTarget(srcFile) + self.WhatIsTarget(destFile) == 2:
-			return True if ReturnMD5String(srcFile) == ReturnMD5String(destFile) else False
+			return True if self.ReturnMD5String(srcFile) == self.ReturnMD5String(destFile) else False
 		else:
 			return False
 
@@ -110,4 +130,4 @@ class FileSync:
 		return HashID.hexdigest().upper()
 
 
-FileSync('D:\\ABC', 'D:\\ABC\\ABC')
+FileSync('D:\\ABC.db', 'D:\\ABC\\DEF\\GHIJK')
