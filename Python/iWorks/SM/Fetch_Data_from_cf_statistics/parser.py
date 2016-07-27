8bc00514186c841c383c0dabfcab2bf89eb32866
James Yang <james.yang@servicemax.io>
2016-07-28 00:58:07 +0800

    Update script, not using larget list

--- Python/iWorks/SM/Fetch_Data_from_cf_statistics/parser.py
+++ Python/iWorks/SM/Fetch_Data_from_cf_statistics/parser.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 
-import xlwt
 import json
 import iso8601
 import datetime
@@ -16,7 +15,7 @@ def ParserJSON(stringline):
     mem_quota   = float(json_sample['metrics']['stats']['mem_quota'])/1024/1024
     disk_quota  = float(json_sample['metrics']['stats']['disk_quota'])/1024/1024
     host        = json_sample['metrics']['stats']['host']
-    usage_cpu   = str(round(float(json_sample['metrics']['stats']['usage']['cpu']), 4))
+    usage_cpu   = str(round(float(json_sample['metrics']['stats']['usage']['cpu'])*100, 4))
     usage_mem   = float(json_sample['metrics']['stats']['usage']['mem'])/1024/1024
     usage_disk  = float(json_sample['metrics']['stats']['usage']['disk'])/1024/1024
     usage_time  = datetime.datetime.strftime(iso8601.parse_date(json_sample['metrics']['stats']['usage']['time']), "%Y-%m-%d %H:%M:%S")
@@ -25,9 +24,8 @@ def ParserJSON(stringline):
 
     return (name, uris, port, host, state, uptime, fds_quota, mem_quota, disk_quota, usage_time, usage_cpu, usage_mem, usage_disk)
 
-
 # Define
-SampleLog = 'sample.log'
+SampleLog = 'CF_Statistics.log'
 Outputter = 'cf_statistics_resource.xlsx'
 sheetname = 'Statistic'
 workbook  = xlsxwriter.Workbook(Outputter)
@@ -35,39 +33,34 @@ worksheet = workbook.add_worksheet(sheetname)
 retResult = []
 LineCount = 2
 
+# Add Title
+header = [  'name', 'uris', 'port', 'host', 'state', 'uptime',
+            'fds_quota', 'mem_quota (MB)', 'disk_quota (MB)',
+            'usage_time', 'usage_cpu (%)', 'usage_mem (MB)', 'usage_disk (MB)']
+formatter_header = workbook.add_format({'bold': 1, 'font_name': 'Courier New', 'font_size': 11})
+formatter_content  = workbook.add_format({'bold': 1, 'font_name': 'Courier New', 'font_size': 9})
+worksheet.write('A1', 'CF STATISTICS REPORT', formatter_header)
+worksheet.write_row('A2', header, formatter_header)
 
 # Generate data
 with open(SampleLog) as f:
     for i in f.readlines():
-        retResult.append(ParserJSON(i.strip('\n')))
-
-
-# Add Title
-header = [  'name', 'uris', 'port', 'host', 'state', 'uptime',
-            'fds_quota', 'mem_quota (MB)', 'disk_quota (MB)',
-            'usage_time', 'usage_cpu', 'usage_mem (MB)', 'usage_disk (MB)']
-formatter_header = workbook.add_format({'bold': 1, 'font_name': 'Courier New', 'font_size': 11})
-worksheet.write('A1', 'CF STATISTICS REPORT', formatter_header)
-worksheet.write_row('A2', header, formatter_header)
-
-
-# Add Data
-formatter_content  = workbook.add_format({'bold': 1, 'font_name': 'Courier New', 'font_size': 9})
-for i in retResult:
-    worksheet.write(LineCount, 0,  i[0],  formatter_content)
-    worksheet.write(LineCount, 1,  i[1],  formatter_content)
-    worksheet.write(LineCount, 2,  i[2],  formatter_content)
-    worksheet.write(LineCount, 3,  i[3],  formatter_content)
-    worksheet.write(LineCount, 4,  i[4],  formatter_content)
-    worksheet.write(LineCount, 5,  i[5],  formatter_content)
-    worksheet.write(LineCount, 6,  i[6],  formatter_content)
-    worksheet.write(LineCount, 7,  i[7],  formatter_content)
-    worksheet.write(LineCount, 8,  i[8],  formatter_content)
-    worksheet.write(LineCount, 9,  i[9],  formatter_content)
-    worksheet.write(LineCount, 10, i[10], formatter_content)
-    worksheet.write(LineCount, 11, i[11], formatter_content)
-    worksheet.write(LineCount, 12, i[12], formatter_content)
-    LineCount += 1
+        if i.find('{"instance_index"') >= 0:
+            j = ParserJSON(i.strip('\n'))
+            worksheet.write(LineCount, 0,  j[0],  formatter_content)
+            worksheet.write(LineCount, 1,  j[1],  formatter_content)
+            worksheet.write(LineCount, 2,  j[2],  formatter_content)
+            worksheet.write(LineCount, 3,  j[3],  formatter_content)
+            worksheet.write(LineCount, 4,  j[4],  formatter_content)
+            worksheet.write(LineCount, 5,  j[5],  formatter_content)
+            worksheet.write(LineCount, 6,  j[6],  formatter_content)
+            worksheet.write(LineCount, 7,  j[7],  formatter_content)
+            worksheet.write(LineCount, 8,  j[8],  formatter_content)
+            worksheet.write(LineCount, 9,  j[9],  formatter_content)
+            worksheet.write(LineCount, 10, j[10], formatter_content)
+            worksheet.write(LineCount, 11, j[11], formatter_content)
+            worksheet.write(LineCount, 12, j[12], formatter_content)
+            LineCount += 1
 
 # Chart #1
 chartsheet1 = workbook.add_chartsheet('CPU')
@@ -81,7 +74,7 @@ chart1.add_series({
 })
 chart1.set_title ({'name': 'Usage of CPU'})
 chart1.set_x_axis({'name': 'usage_time'})
-chart1.set_y_axis({'name': 'usage_cpu'})
+chart1.set_y_axis({'name': 'usage_cpu (%)'})
 chart1.set_style(10)
 chartsheet1.set_chart(chart1)
 
@@ -104,7 +97,7 @@ chart2.add_series({
 })
 chart2.set_title ({'name': 'Usage of MEM'})
 chart2.set_x_axis({'name': 'usage_time'})
-chart2.set_y_axis({'name': 'usage_mem'})
+chart2.set_y_axis({'name': 'usage_mem (MB)'})
 chart2.set_style(10)
 chartsheet2.set_chart(chart2)
 
@@ -127,7 +120,7 @@ chart3.add_series({
 })
 chart3.set_title ({'name': 'Usage of Disk'})
 chart3.set_x_axis({'name': 'usage_time'})
-chart3.set_y_axis({'name': 'usage_disk'})
+chart3.set_y_axis({'name': 'usage_disk (MB)'})
 chart3.set_style(10)
 chartsheet3.set_chart(chart3)
 
